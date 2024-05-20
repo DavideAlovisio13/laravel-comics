7551ea4a177434c6b0afa8c7f434fa2b24f7ad53
DavideAlovisio13 <alovisiod@gmail.com>
2024-05-20 15:06:24 +0200

    (header.blade) insert logo.png

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    $comics = config('db');
-    return view('home', compact('comics'));
+    $comics = config('db.comics');
+    $menus = config('db.menus');
+    return view('home', compact('comics'), compact('menus'));
 })->name('home');
